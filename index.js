c4f413d4e1facb884f3d1ca66f3788e262775ef1
nishulabiba <nishlabiba2gmail.com>
2023-08-27 00:42:41 +0600

    api created

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require("cors")
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+
+const chefData = require("./data/chefData.json")
+
+app.get('/', (req, res)=>{
+
+    res.send("This site is running")
+
+})
+
+app.get("/chefdetails", (req, res)=>{
+    res.send(chefData)
+} )
+
+app.listen(port, ()=>{
+    console.log(`chefData api running on port ${port}`);
+})
